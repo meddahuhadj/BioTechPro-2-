61a5ebda929f22673c0b1232a3636a3914aa5758
meddahuhadj <116365523+meddahuhadj@users.noreply.github.com>
2026-04-21 20:33:12 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = 'biotechpro-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
